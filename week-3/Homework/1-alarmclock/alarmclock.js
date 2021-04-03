d483e765bb4fd5105d79d1767e3468e7f626ec0c
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-04-03 18:35:00 +0200

    todo completado

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,4 +1,46 @@
-function setAlarm() {}
+/*
+
+cuando ponga set alarm tienen que pasar dos cosas:
+1- el numero puesto en input el titulo tiene que cambiar a Tiempo Restante 00:10
+2- cada segundo que pase el el titulo debe de bajar un segundo
+
+cuando el titulo diga Tiempo Restante 00:00 la alarma debe tocar un sonido. puedes hacer que esto 
+pase usando playAlarm()
+
+puedes parar la alarma presionando  Stop Alarm btn
+
+*/
+let interval = 0
+function setAlarm() {
+  clearInterval(interval)
+  let contador = 0 // necesitamos esto para ejecutar la cuenta regresiva de manera correct
+  let tiempoRestante = document.getElementById('alarmSet')//necesitamos definir cual es valor que vamos a poner en time remaini...
+  let tiempoRestante1 =tiempoRestante.value - contador //ahora a este valor le tenemos que restar el contador (por ejemplo 10-1=9)
+  let conteo = document.getElementById('timeRemaining')//tenemos que intervenir ahora el time reamining
+  interval = setInterval(countdown,1000)//esto es lo que hace que countdown se repita cada mil milisecundos (1sg)
+  function countdown() {//vamos a empezar a crear el loop
+      contador++;//al contador le vamos a sumar 1 por cada vuelta
+      cuentaRegresiva = tiempoRestante1 - contador
+      conteo.innerText =`Time Remaining: ${cuentaRegresiva} sec`//aqui definimos todo... 10-1=9 10-2=8
+      if (cuentaRegresiva === 0){
+        playAlarm()//cuando llegue a zero play the sound
+        clearInterval(interval)//aqui paramos el loop
+      }
+      
+  }
+      
+      
+      
+      
+      
+      //cambiar el tema cada 1 segundo cuando llegue a zero
+      //poner el tema normal cuando stop alarm
+
+  
+
+  
+
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -16,10 +58,12 @@ function setup() {
 
 function playAlarm() {
   audio.play();
+  
 }
 
 function pauseAlarm() {
   audio.pause();
+
 }
 
 window.onload = setup;
--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -1,1 +1,91 @@
 // Write your code here
+//crear un array con todas las fotos
+//hacer que cada vez que se presione el boton ff vaya en direccion uno mas en cada loop del array
+//hacer que cada vez que se presione el boton rr vaya en direccion uno menos en cada loop del array
+
+
+//vamos por el array
+
+let fotos = ["./fotos/toño (1).jpg","./fotos/toño (2).jpg","./fotos/toño (3).jpg","./fotos/toño (4).jpg",
+    "./fotos/toño (5).jpg","./fotos/toño (6).jpg","./fotos/toño (7).jpg",
+    "./fotos/toño (8).jpg","./fotos/toño (9).jpg","./fotos/toño (10).jpg",
+    "./fotos/toño (11).jpg","./fotos/toño (12).jpg","./fotos/toño (13).jpg",
+    "./fotos/toño (14).jpg","./fotos/toño (15).jpg","./fotos/toño (16).jpg",
+    "./fotos/toño (17).jpg","./fotos/toño (18).jpg",
+    "./fotos/toño (20).jpg","./fotos/toño (21).jpg","./fotos/toño (22).jpg"]
+
+//array creado con todas las ubicaciones de las fotos, debe de haber una manera mejor... pero que diablos
+
+//vamos a seleccionar los botones sobre los que vamos a ejecutar las acciones
+
+const forwardbtn = document.getElementById('forward');
+const rewindtbn = document.getElementById('back');
+const autoRewindbtn = document.getElementById('autoback');
+const autoForwardbtn = document.getElementById('autoforward');
+const stopbtn = document.getElementById('stop');
+
+
+//necesitamos saber la linea que vamos a modificar el nombre
+
+let photoName = document.getElementById('img');
+//photoName.innerText= `<img id="img" src="${fotos[0]}" class="rounded img-thumbnail" width="400px"></img>`
+
+//creamos las formulas para que vaya cambiando el nombre del index de la foto
+index = 0
+
+function haciadelante() {
+    if(index === fotos.length-1) {
+       index=-1
+    }
+    index++;
+    photoName.src= fotos[index]
+  
+}
+
+function haciatras() {
+    if(index === 0){
+        index=fotos.length
+    }
+    index--;
+    photoName.src= fotos[index]
+}
+
+//ahora la funcion para cortar todo los loops
+
+let internalref = 0
+
+
+function stopAll() {
+    clearInterval(internalref)
+}
+
+
+//ahora solo queda asignar cada funcion a cada boton
+
+forwardbtn.addEventListener('click', () => { //boton hacia adelante sin loop
+    haciadelante()
+})
+
+rewindtbn.addEventListener('click', () => { //boton hacia atrás sin loop
+    haciatras()
+    
+})
+
+autoForwardbtn.addEventListener('click',()=>{ //boton hacia adelante con loop
+    clearInterval(internalref)
+    internalref=setInterval(haciadelante,2000)
+})
+
+autoRewindbtn.addEventListener('click',()=>{ //boton hacia atrás con loop
+    clearInterval(internalref)
+    internalref=setInterval(haciatras,2000)
+})
+
+stopbtn.addEventListener('click',()=>{ //boton stop, para cualquier loop que se esté ejecutando
+    stopAll()
+})
+
+
+    
+
+
